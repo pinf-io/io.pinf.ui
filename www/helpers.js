6c8b4b863cfd4dd1df21e1dce3ab7346120a6657
Christoph Dorn <christoph@christophdorn.com>
2014-08-20 13:35:16 -0700

    show log dialog

--- www/helpers.js
+++ www/helpers.js
@@ -12,8 +12,33 @@ define([
 	function renderWidgetIntoDomId(domId, widgetId) {
 		var domNode = $("#" + domId);
 		domNode.attr("x-widget", widgetId);
-		return FIREWIDGETS.scan(domNode.parent());
+		return FIREWIDGETS.scan(domNode, null, {
+			scanSelf: true
+		});
 	}
+
+	var lastWidgets = [];
+	function showLogDialog(logPath, options) {
+		options = options || {};
+		var widget = $("#show-log-dialog-widget");
+		if (widget.length === 1) {
+			widget.remove();
+		}
+		$("body").append('<div id="show-log-dialog-widget">' + JSON.stringify({
+			logPath: logPath,
+			updateUrlHash: options.updateUrlHash || false
+		}) + '</div>');
+		return renderWidgetIntoDomId("show-log-dialog-widget", "io.pinf.server.log/log-dialog").then(function (widgets) {
+			if (lastWidgets) {
+			    while (lastWidgets.length > 0) {
+			        lastWidgets.shift().destroy();
+			    }
+			}
+		    widgets.reverse();
+		    lastWidgets = widgets;
+		});
+	}
+
 
 	return {
 		API: {
@@ -21,7 +46,8 @@ define([
 			BACKBONE: BACKBONE
 		},
 		getViews: getViews,
-		renderWidgetIntoDomId: renderWidgetIntoDomId
+		renderWidgetIntoDomId: renderWidgetIntoDomId,
+		showLogDialog: showLogDialog
 	};
 
 });
